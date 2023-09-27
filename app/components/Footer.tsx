4be9c127986cc769ee97cd3444cd02e4be4f9118
Leo0nid <121210862+Leo0nid@users.noreply.github.com>
2023-09-27 14:35:54 +0600

    'add_navbar'

---
+++ app/components/Footer.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+const Footer = () => {
+  return (
+    <footer className='flex flex-column text-black-100'>
+      
+    </footer>
+  )
+}
+
+export default Footer
